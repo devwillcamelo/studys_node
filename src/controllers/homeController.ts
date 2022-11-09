7d3be8725a00d70faeccda401fc4ca10fdf42dc0
William Camelo <devwillcamelo@gmail.com>
2022-11-08 23:02:03 -0300

    four commit

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,7 +1,67 @@
 import { Request, Response } from "express";
 import { Product } from '../models/Product'
+import { sequelize } from "../instances/mysql";
+import { User } from '../models/User';
+import { Op } from 'sequelize';
 
-export const home = ( req: Request, res: Response ) => {
+export const home = async ( req: Request, res: Response ) => {
+        //update
+        await User.update({ name: 'Seu Chico', age: 56 /*age: 18*/}, {
+            where:{
+                id: 4
+
+                /*age: {
+                    [Op.lt]: 18 *atualizar todos para 18 anos
+                }*/
+            }
+        })
+    
+    
+        // build + save
+        const userNew = User.build({
+                name: 'Fulaninho',
+                age: 25
+        });
+        await userNew.save();  //comando para salvar alterações
+        
+        // create
+        /*const userNew = await User.create({
+                name: 'Ciclano',
+                age: 39
+        });*/
+        
+    
+        //let users = await User.findAll();
+        let users = await User.findAll({
+                where: { 
+                    //name: Paulo, id: 3
+                    //age: X, name: Y
+                    /*[Op.or]: [
+                        {age: 55},
+                        {//name: Ciclano}
+                    ]*/
+                    //Op.gt, Op.gte, Op.lt, Op.let
+                    /* GT = Greather Than
+                        GTE = Greather Than Equal
+                        LT = Lower Than
+                        LTE = Lower Than Equal*/
+                },
+                order:[
+                    ['name', 'ASC'],
+                    ['age', 'DESC']            
+                ],
+                offset: 1, //pular itens
+                limit: 2    // limitar exibição
+            });
+
+    try {
+        await sequelize.authenticate();
+        console.log("Conexão ok");
+    }
+    catch(error) {
+        console.log("Deu erro: ", error)
+    }
+    
     let user = {
         name: 'Fulano de Tal',
         age: 90
